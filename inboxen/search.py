d9b0f961196c2b1d0923716e017bb61dfa1bac2f
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2017-06-11 00:29:27 +0100

    WIP

--- inboxen/search.py
+++ inboxen/search.py
@@ -20,6 +20,7 @@
 import re
 
 from django.core import exceptions
+from django.db.models import Q
 
 from watson import search
 
@@ -33,19 +34,50 @@ class EmailSearchAdapter(search.SearchAdapter):
     trunc_to_size = 2 ** 20  # 1MB. Or two copies of 1984
 
     def get_bodies(self, obj):
-        """Return a queryset of text/* bodies for given obj"""
-        from inboxen.models import Body
+        """Return a queryset of text/plain bodies for given obj"""
+        from inboxen.models import Body, PartList
 
         data = Body.objects.filter(
             partlist__email__id=obj.id,
+            # current part should be plaintext
             partlist__header__name__name="Content-Type",
-            partlist__header__data__data__startswith="text/",
+            partlist__header__data__data__startswith="text/plain",
+        ).filter(
+            # check parent
+            Q(
+                partlist__parent__header__name__name="Content-Type",
+                partlist__parent__header__data__data__startswith="multipart",
+            ) | Q(partlist__parent__isnull=True),
         )
 
+        # TODO work out how to exclude n subtrees all in one database query
+
+        excluded_part_values = PartList.objects.filter(
+            header__name__name="Content-Type",
+            header__data__data__startswith="message/"
+        ).values_list('lft', 'rght')
+
+        if len(excluded_part_values) > 0:
+            q = Q()
+            for lft, rght in excluded_part_values:
+                q = q | Q(
+                    partlist__lft__gte=lft,
+                    partlist__rght__lte=rght,
+                )
+
+            # first item in q is not required
+            del q.children[0]
+
+            if len(q.children) > 0:
+                data.exclude(q)
+
+
         if len(data) == 0:
-            data = Body.objects.filter(partlist__email__id=obj.id)
-            data = data.exclude(partlist__header__name__name="Content-Type")
-            data = data.exclude(partlist__header__name__name="MIME-Version")
+            data = Body.objects.filter(partlist__email__id=obj.id).exclude(
+                partlist__header__name__name="Content-Type",
+            ).exclude(
+                partlist__header__name__name="MIME-Version",
+            )
 
         return data
 
@@ -83,7 +115,7 @@ class EmailSearchAdapter(search.SearchAdapter):
             return u""
 
     def get_description(self, obj):
-        """Fetch first text/* body for obj, reading up to `trunc_to_size` bytes
+        """Fetch first text/plain body for obj, reading up to `trunc_to_size` bytes
         """
         try:
             body = self.get_bodies(obj)[0]
@@ -93,7 +125,7 @@ class EmailSearchAdapter(search.SearchAdapter):
         return unicode_damnit(body.data[:self.trunc_to_size], self.get_body_charset(obj, body))
 
     def get_content(self, obj):
-        """Fetch all text/* bodies for obj, reading up to `trunc_to_size` bytes"""
+        """Fetch all text/plain bodies for obj, reading up to `trunc_to_size` bytes"""
         data = []
         size = 0
         for body in self.get_bodies(obj):
@@ -103,7 +135,7 @@ class EmailSearchAdapter(search.SearchAdapter):
             if remains <= 0:
                 break
             elif remains < body.size:
-                data.append(unicode_damnit(body.data[:remains], self.get_body_charset(obj, body))
+                data.append(unicode_damnit(body.data[:remains], self.get_body_charset(obj, body)))
                 break
             else:
                 data.append(unicode_damnit(body.data, self.get_body_charset(obj, body)))
